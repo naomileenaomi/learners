0c63d94bcfc193a3d258aa46c18d5f032b2141d3
naomileenaomi <naomileenaomi@gmail.com>
2023-03-04 01:48:02 -0500

    removes some unneeded imports and fixes whitespace

--- test_classes.py
+++ test_classes.py
@@ -1,6 +1,4 @@
-from conftest import nominalizer_terminal_input1
 import main
-import pytest
 
 def test_permuted_value_equality(semantic_terminal1, nominalizer_terminal1):
     assert semantic_terminal1.values == nominalizer_terminal1.values
